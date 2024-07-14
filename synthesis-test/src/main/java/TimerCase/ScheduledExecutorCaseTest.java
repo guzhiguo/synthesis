0938e9ad94435a56cb32b47c6bdd72b1d1b6b35f
guzhiguo <248461341@qq.com>
2024-07-14 12:40:36 +0800

    代码注释

--- synthesis-test/src/main/java/TimerCase/ScheduledExecutorCaseTest.java
+++ synthesis-test/src/main/java/TimerCase/ScheduledExecutorCaseTest.java
@@ -7,6 +7,10 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * ScheduledExecutor 其设计的思想是 每个被调度的任务都会由线程池中的一个线程去执行 因此任务是并发执行的
+ * 相互之间不会受到干扰 需要注意的是 只有当任务的执行时间来时 ScheduledExecutor才回真正启动一个线程
+ * 其余时间ScheduledExecutor都是在轮询任务的状态
+ *
  * @author: bill
  * @create: 2024/07/14 12:31
  */
