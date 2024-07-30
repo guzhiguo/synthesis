e77c15b3d810267b750359ee3140d657292a327f
guzhiguo <248461341@qq.com>
2024-07-30 17:54:49 +0800

    test

---
+++ synthesis-test/src/main/java/Aggregate/HashTableTest.java
@@ -0,0 +1,15 @@
+package Aggregate;
+
+import java.util.Hashtable;
+
+/**
+ * @author: bill
+ * @create: 2024/07/29 15:43
+ */
+public class HashTableTest {
+    public static void main(String[] args) {
+        Hashtable hashtable = new Hashtable();
+        hashtable.put("A","AA");
+        System.out.printf("hashTable:"+hashtable.toString());
+    }
+}
